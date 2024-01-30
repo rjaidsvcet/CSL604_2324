a51d5aea30d7233f7ea7931015d060045c59e506
rjaidsvcet <raunak.joshi@vcet.edu.in>
2024-01-30 12:08:59 +0530

    Logistic Regression Changes

--- Experiment_03/logisticRegression.py
+++ Experiment_03/logisticRegression.py
@@ -15,14 +15,16 @@ class LogisticRegression:
         return self.params
     
     def predict (self, X):
+        y_hat = list ()
         bias_testing = np.ones (len (X))
         X_test = np.c_[bias_testing, X]
         z = X_test @ self.params
         sigmoid = 1 / (1 + np.exp (-z))
-        if sigmoid >= 0.5:
-            y_hat = 1
-        else:
-            y_hat = 0
+        for _ in range (len (sigmoid)):
+            if sigmoid[_] >= 0.5:
+                y_hat.append (1)
+            else:
+                y_hat.append (0)
         return sigmoid, y_hat
 
 if __name__ == '__main__':
@@ -32,5 +34,5 @@ if __name__ == '__main__':
     model = LogisticRegression ()
     parameters = model.fit (X, y)
 
-    sig, y_pred = model.predict ([[3.75]])
+    sig, y_pred = model.predict ([[0.75]])
     print (f'The predicted outcome is {y_pred} and calculated sigmoid value is {sig}')
