19e47f0c8e4ab583aaf349e8344ef5cf1aba240f
rjaidsvcet <raunak.joshi@vcet.edu.in>
2024-02-22 15:13:32 +0530

    Formula Combined

--- Experiment_02/multipleLinearRegression.py
+++ Experiment_02/multipleLinearRegression.py
@@ -1,16 +1,13 @@
 import numpy as np
 
-class MultipleRegression:
+class LinearRegression:
     def __init__ (self):
         self.params = np.zeros(int(np.random.random()), float)[:,np.newaxis]
 
     def fit (self, X, y):
         bias = np.ones (len (X))
         X_bias = np.c_[bias, X]
-        inner_part = np.transpose (X_bias) @ X_bias
-        inverse_part = np.linalg.inv (inner_part)
-        outer_part = inverse_part @ np.transpose (X_bias)
-        lse = outer_part @ y
+        lse = (np.linalg.inv (np.transpose(X_bias) @ X_bias) @ np.transpose (X_bias)) @ y
         self.params = lse
         return self.params
     
@@ -19,9 +16,8 @@ class MultipleRegression:
         X_test = np.c_[bias_testing, X]
         y_hat = X_test @ self.params
         return y_hat
-    
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     X = np.array ([
         [1, 4],
         [2, 5],
@@ -31,8 +27,9 @@ if __name__ == '__main__':
 
     y = np.array ([1, 6, 8, 12])
 
-    model = MultipleRegression ()
+    model = LinearRegression ()
     parameters = model.fit (X, y)
-    print (f'The parameters are {parameters}')
+    print (f'The parameters for the model are : {parameters}')
+
     y_pred = model.predict ([[5, 3]])
-    print (f'The predicted value is {y_pred}')
+    print (f'The predicted outcome is : {y_pred}')
